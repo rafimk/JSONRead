f390300328766d346a9c91f91c62a4c273c80655
Muhammed Rafi <m.rafi@afad.gov.ae>
2022-06-17 10:26:58 +0400

    Initial

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using JSONRead;
+using Newtonsoft.Json;
+
+Console.WriteLine("Started!");
+
+var json = File.ReadAllText("users.json");
+var dict = JsonConvert.DeserializeObject<List<UsersData>>(json);
+
+foreach(var item in dict)
+{
+    Console.WriteLine("name ",item.Name);    
+}
+
+Console.WriteLine("Completed!");
---
+++ UsersData.cs
@@ -0,0 +1,10 @@
+namespace JSONRead
+{
+    public class UsersData
+    {
+        public int Id { get; set;}
+        public string Name { get; set;}
+
+        public int Age { get; set;}
+    }
+}
